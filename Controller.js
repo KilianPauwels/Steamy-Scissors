9b2ccfc4f8115e5044da093b8b623ece78c30d8f
KilianPauwels <Kilianpauwels@gmail.com>
2016-03-22 11:46:39 +0100

    basic angular stuff

--- Controller.js
+++ Controller.js
@@ -1,1 +1,27 @@
-var app = angular.module("myapp", []);
+var app= angular.module("myapp",[]);
+app.controller("artistController", ["$scope", function($scope){
+	$scope.title = "Top Artists";
+	$scope.artists = [
+	{
+		name: "Taylor Swift",
+		age: 27,
+		description: "beste vrouwelijke artiest ooit",
+		facebooklink: "https://www.facebook.com/TaylorSwift/?fref=ts"
+	},
+	{
+		name: "Kanye West",
+		age: 69,
+		description: "loser",
+		facebooklink: "https://www.facebook.com/pages/Kanye-West/203406986358515?fref=ts&rf=107777555911981"
+	},
+	{
+		name: "Wart Scheppers",
+		age: 19,
+		description: "junk"
+	},
+	{
+		name: "Sven Bollaerts",
+		age: 420,
+		description: "420 blaze it"
+	}]
+}]);
