06a215d07fbf65bfd415dc8a3d9a46f404e3b41e
KilianPauwels <Kilianpauwels@gmail.com>
2016-03-23 16:03:02 +0100

    kleine (slechte) about page geschreven

--- Controller.js
+++ Controller.js
@@ -13,12 +13,14 @@ app.controller("artistController", ["$scope", function($scope){
 		name: "Kanye West",
 		age: 69,
 		description: "loser",
-		facebooklink: "https://www.facebook.com/pages/Kanye-West/203406986358515?fref=ts&rf=107777555911981"
+		facebooklink: "https://www.facebook.com/pages/Kanye-West/203406986358515?fref=ts&rf=107777555911981",
+		img: "https://pbs.twimg.com/profile_images/585565077207678977/N_eNSBXi.jpg"
 	},
 	{
 		name: "Wart Scheppers",
 		age: 19,
-		description: "junk"
+		description: "junk",
+		img:"https://scontent-bru2-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1465376_10205959416805872_6438716955300965378_n.jpg?oh=ac49fdbacc8378cd0b091357c49c0734&oe=578B3436"
 	},
 	{
 		name: "Sven Bollaerts",
