c8db1b58f706d3d2f48a54d4ce24430fc0c49f08
KilianPauwels <kilianpauwels@gmail.com>
2016-04-17 23:01:39 +0200

    ordering van api's

--- addArtistController.js
+++ addArtistController.js
@@ -8,19 +8,12 @@ app.controller("myController", [function($scope,$http){
 		img:'',
 		name:''
 	};
-	var data = new Firebase("onzedatabase.com/data")
+	var data = new Firebase("onzedatabase/data")
 
 	this.upload = function(){
 		console.log("user clicked upload", this.artist);
 		var newDataPush = data.push(this.artist);
 	};
 
-		/*$http.get("https://burning-torch-1101.firebaseio.com/.json")
-		.succes(function(results){
-			console.log( "in succes");
-
-		})
-		.error(function(error){
-			console.log(error);
-		})*/
+		
 }]);
