81d71442bddcdef8c15dec65c7434f994bbb83b7
KilianPauwels <kilianpauwels@gmail.com>
2016-04-18 20:41:34 +0200

    ngRouting in orde

---
+++ routing.js
@@ -0,0 +1,66 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider){
+	$routeProvider
+
+		.when("/start",{
+			templateUrl : "views/start.html",
+			controller : "startController"
+		})
+
+		.when("/about", {
+			templateUrl : "views/about.html",
+			controller : "aboutController"
+		})
+
+		.when("/addArtists", {
+			templateUrl : "views/addArtists.html",
+			controller : "addArtistsController"
+		});
+});
+
+app.controller("startController", function($scope,$http){
+	$scope.title = "Top artists";
+	$scope.artists =[];
+	$scope.namen = [];
+
+		$http.get("onzedatabase/.json")
+		.success(function(results){
+			$scope.artists = results.data;
+			console.log("in succes");
+		
+				for(var i=0; i < $scope.artists.length; i++){
+				var current = $scope.artists[i].name;
+				if($scope.namen.indexOf(current)<0){
+				$scope.namen.push(current);
+			}
+		}
+		console.log(results.data);
+		})
+		.error(function(error){
+			console.log(error);
+		})
+});
+
+app.controller("aboutController",function($scope){
+	$scope.message = "about";
+});
+
+app.controller("addArtistsController", [function($scope,$http){
+	
+	this.artist = {
+		age: '',
+		description:'',
+		facebooklink:'',
+		img:'',
+		name:''
+	};
+	var data = new Firebase("onzedatabase/data")
+
+	this.upload = function(){
+		console.log("user clicked upload", this.artist);
+		var newDataPush = data.push(this.artist);
+	};
+
+		
+}]);
