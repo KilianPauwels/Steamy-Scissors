8d3848c60ebeb5c1c55d828462bdd872ca4c5e94
Ward Schepers <wardjeschepers1@hotmail.com>
2016-05-28 17:26:00 +0100

    Wikipedia api voor artist paginas

---
+++ js/artistScript.js
@@ -0,0 +1,6 @@
+
+$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=Taylor_Swift&prop=text&format=json&callback=?', function(json) { 
+    $('#wikiInfo').html(json.parse.text['*']); 
+    $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
+    $("#wikiInfo").find("a").attr("target", "_blank"); 
+  });
