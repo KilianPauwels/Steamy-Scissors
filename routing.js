5d77cc20dec0cbe7c29b52b661c9896ea47e297a
Ward Schepers <wardjeschepers1@hotmail.com>
2016-05-29 15:29:28 +0100

    WHY WON'T YOU WORK

--- routing.js
+++ routing.js
@@ -75,6 +75,14 @@ app.controller("artistController", function($scope,$http){
 
 app.controller("AboutArtistController", function($scope,$http){
 
+$(document).ready(function () {
+console.log("dit werkt")
+$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&section=0&page=Taylor_Swift&prop=text&format=json&callback=?', function(json) { 
+    $('#information').html(json.parse.text['*']); 
+    $("#information").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
+    $("#information").find("a").attr("target", "_blank"); 
+  });
+});
 
 });
 
