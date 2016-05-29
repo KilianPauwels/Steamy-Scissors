633d6c2cb9455a0d88717176c15ee54717c3f8d3
Ward Schepers <wardjeschepers1@hotmail.com>
2016-05-29 14:35:16 +0100

    Databass name

--- routing.js
+++ routing.js
@@ -9,7 +9,11 @@ app.config(function($routeProvider){
 		})
 			.when("/artists",{
 			templateUrl : "views/artists.html",
-			controller : "	artistController"
+			controller : "artistController"
+		})
+				.when("/AboutArtist",{
+			templateUrl : "views/AboutArtist.html",
+			controller : "AboutArtistController"
 		})
 
 		.when("/about", {
@@ -20,11 +24,6 @@ app.config(function($routeProvider){
 		.when("/addArtists", {
 			templateUrl : "views/addArtists.html",
 			controller : "addArtistsController"
-		})
-
-		.when("/showArtists",{
-			templateUrl: "views/showArtists.html",
-			controller: "showArtistsController"
 		});
 });
 
@@ -33,7 +32,7 @@ app.controller("startController", function($scope,$http){
 	$scope.artists =[];
 	$scope.namen = [];
 
-		$http.get("database/.json")
+		$http.get("https://burning-torch-1101.firebaseio.com/.json")
 		.success(function(results){
 			$scope.artists = results.data;
 			console.log("in succes");
@@ -51,6 +50,34 @@ app.controller("startController", function($scope,$http){
 		})
 });
 
+app.controller("artistController", function($scope,$http){
+	$scope.title = "artists";
+	$scope.artists =[];
+	$scope.namen = [];
+
+		$http.get("DATABASE/.json")
+		.success(function(results){
+			$scope.artists = results.data;
+			console.log("in succes");
+		
+				for(var i=0; i < $scope.artists.length; i++){
+				var current = $scope.artists[i].name;
+				if($scope.namen.indexOf(current)<0){
+				$scope.namen.push(current);
+			}
+		}
+		console.log(results.data);
+		})
+		.error(function(error){
+			console.log(error);
+		})
+});
+
+app.controller("AboutArtistController", function($scope,$http){
+
+
+});
+
 app.controller("aboutController",function($scope){
 	$scope.message = "about";
 });
@@ -64,7 +91,7 @@ app.controller("addArtistsController", [function($scope,$http){
 		img:'',
 		name:''
 	};
-	var data = new Firebase("database/data")
+	var data = new Firebase("DATABASE/data")
 
 	this.upload = function(){
 		console.log("user clicked upload", this.artist);
@@ -72,33 +99,4 @@ app.controller("addArtistsController", [function($scope,$http){
 	};
 
 		
-}]);
-
-app.controller("showArtistsController", [function($scope,$http){
-
-	$("#showArtistsButton").click(function playSomeSound(genre){
-	SC.get('/tracks',{
-		genres: genre
-	}, function(tracks){
-			var random = Math.floor(Math.random()*49);
-			SC.oEmbed(tracks[random].uri, {autoplay: true}, document.getElementById('target'));
-		});
-	});
-
-	$(document).ready(function(){
-		SC.initialize({
-			client_id: '5c4bf4a850b4ac822bc9e6000203d52f'
-	});
-
-
-	var menuLinks = document.getElementById('genre');
-		for(var i = 0; i < menuLinks.length; i++){
-			var menuLink = menuLinks[i];
-			menuLinks[i].onclick = function(e){
-				e.preventDefault();
-				playSomeSound(menuLink.innerHTML);
-			};
-		}
-});
-
 }]);
